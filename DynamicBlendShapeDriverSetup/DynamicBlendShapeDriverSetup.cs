33c88fe8f9c7482fa3a19c3825b1f49b14bf1150
Arne Kiesewetter <banane9@live.de>
2022-08-09 19:23:52 +0200

    Add project files.

---
+++ DynamicBlendShapeDriverSetup/DynamicBlendShapeDriverSetup.cs
@@ -0,0 +1,108 @@
+﻿using BaseX;
+using FrooxEngine;
+using FrooxEngine.UIX;
+using HarmonyLib;
+using NeosModLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace DynamicBlendShapeDriverSetup
+{
+    public class DynamicBlendShapeDriverSetup : NeosMod
+    {
+        public static ModConfiguration Config;
+
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<bool> EnableCleanButton = new ModConfigurationKey<bool>("EnableCleanButton", "Enable adding a button to the DynamicBlendShapeDriver component to remove BlendShapes not present on the renderer linked to it.", () => true);
+
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<bool> EnableSetupButton = new ModConfigurationKey<bool>("EnableSetupButton", "Enable adding a button to the DynamicBlendShapeDriver component to setup all BlendShapes from the renderer linked to it.", () => true);
+
+        public override string Author => "Banane9";
+        public override string Link => "https://github.com/Banane9/NeosDynamicBlendShapeDriverSetup";
+        public override string Name => "DynamicBlendShapeDriverSetup";
+        public override string Version => "1.0.0";
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony($"{Author}.{Name}");
+            Config = GetConfiguration();
+            Config.Save(true);
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(DynamicBlendShapeDriver), nameof(DynamicBlendShapeDriver.BuildInspectorUI))]
+        private static class DynamicBlendShapeDriverPatch
+        {
+            private static readonly MethodInfo updateBlendShapesMethod = typeof(DynamicBlendShapeDriver).GetMethod("UpdateBlendShapes", AccessTools.allDeclared);
+
+            private static HashSet<string> CleanBrokenBlendShapes(DynamicBlendShapeDriver blendShapeDriver)
+            {
+                var renderer = blendShapeDriver.Renderer.Target;
+
+                if (renderer.BlendShapeWeights.Count < renderer.BlendShapeCount)
+                    renderer.BlendShapeWeights.AddRange(Enumerable.Repeat(0f, renderer.BlendShapeCount - renderer.BlendShapeWeights.Count));
+
+                var availableBlendShapes = new HashSet<string>(Enumerable.Range(0, renderer.BlendShapeCount).Select(i => renderer.BlendShapeName(i)));
+
+                blendShapeDriver.BlendShapes.RemoveAll(shape => !availableBlendShapes.Contains(shape.BlendShapeName.Value));
+
+                return new HashSet<string>(blendShapeDriver.BlendShapes.Select(shape => shape.BlendShapeName.Value));
+            }
+
+            private static void Postfix(DynamicBlendShapeDriver __instance, UIBuilder ui)
+            {
+                if (Config.GetValue(EnableSetupButton))
+                {
+                    var setup = ui.Button("Setup BlendShapes from Renderer", color.Pink);
+
+                    setup.LocalPressed += (bttn, data) =>
+                    {
+                        if (!(__instance.Renderer.Target is SkinnedMeshRenderer renderer))
+                            return;
+
+                        SetupBlendshapesFromRenderer(__instance);
+                        updateBlendShapesMethod.Invoke(__instance, null);
+                    };
+                }
+
+                if (Config.GetValue(EnableCleanButton))
+                {
+                    var clean = ui.Button("Clean up broken BlendShape entries", color.Pink);
+
+                    clean.LocalPressed += (bttn, data) =>
+                    {
+                        if (!(__instance.Renderer.Target is SkinnedMeshRenderer renderer))
+                        {
+                            __instance.BlendShapes.Clear();
+                            return;
+                        }
+
+                        CleanBrokenBlendShapes(__instance);
+                        updateBlendShapesMethod.Invoke(__instance, null);
+                    };
+                }
+            }
+
+            private static void SetupBlendshapesFromRenderer(DynamicBlendShapeDriver blendShapeDriver)
+            {
+                var renderer = blendShapeDriver.Renderer.Target;
+
+                var existingBinds = CleanBrokenBlendShapes(blendShapeDriver);
+
+                for (var i = 0; i < renderer.BlendShapeCount; ++i)
+                {
+                    var name = renderer.BlendShapeName(i);
+                    if (existingBinds.Contains(name))
+                        continue;
+
+                    var shape = blendShapeDriver.BlendShapes.Add();
+                    shape.BlendShapeName.Value = name;
+                    shape.Value.Value = renderer.GetBlendShapeWeight(i);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+Dynamic Blend Shape Driver Setup
+================================
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that adds buttons to the DynamicBlendShapeDriver component to automatically add entries for all BlendShapes on the linked SkinnedMeshRenderer
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+2. Place [DynamicBlendShapeDriverSetup.dll](https://github.com/Banane9/NeosDynamicBlendShapeDriverSetup/releases/latest/download/DynamicBlendShapeDriverSetup.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
